34606ea52981f74e1fde3a74b42bf8b00d1af30b
Andrey Yurchuk <r1430@tut.by>
2026-03-18 20:46:00 +0300

    feat: make product repository async

--- src/domain/product/product.repository.port.ts
+++ src/domain/product/product.repository.port.ts
@@ -1,9 +1,9 @@
 import type { Product, ProductCreateDto, ProductUpdateDto } from "./product.types";
 
 export interface ProductRepository {
-  findAll(): Product[];
-  findById(id: string): Product | undefined;
-  create(dto: ProductCreateDto): Product;
-  update(id: string, dto: ProductUpdateDto): Product | undefined;
-  delete(id: string): boolean;
+  findAll(): Promise<Product[]>;
+  findById(id: string): Promise<Product | undefined>;
+  create(dto: ProductCreateDto): Promise<Product>;
+  update(id: string, dto: ProductUpdateDto): Promise<Product | undefined>;
+  delete(id: string): Promise<boolean>;
 }
